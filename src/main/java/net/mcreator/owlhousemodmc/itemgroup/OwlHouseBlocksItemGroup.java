cb9d5dba7487997e426b417d10f3dad5f0985d81
Blaziken77mw <maswils77@icloud.com>
2021-10-31 18:46:31 -0500

    added creative tabs and blocks folders added creative tab "Owl House blocks" added Glyph workbench block and gui (currently nonfunctional)

---
+++ src/main/java/net/mcreator/owlhousemodmc/itemgroup/OwlHouseBlocksItemGroup.java
@@ -0,0 +1,35 @@
+
+package net.mcreator.owlhousemodmc.itemgroup;
+
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+
+import net.mcreator.owlhousemodmc.block.GlyphWorkbenchBlock;
+import net.mcreator.owlhousemodmc.OwlhousemodmcModElements;
+
+@OwlhousemodmcModElements.ModElement.Tag
+public class OwlHouseBlocksItemGroup extends OwlhousemodmcModElements.ModElement {
+	public OwlHouseBlocksItemGroup(OwlhousemodmcModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		tab = new ItemGroup("tabowl_house_blocks") {
+			@OnlyIn(Dist.CLIENT)
+			@Override
+			public ItemStack createIcon() {
+				return new ItemStack(GlyphWorkbenchBlock.block, (int) (1));
+			}
+
+			@OnlyIn(Dist.CLIENT)
+			public boolean hasSearchBar() {
+				return false;
+			}
+		};
+	}
+	public static ItemGroup tab;
+}
